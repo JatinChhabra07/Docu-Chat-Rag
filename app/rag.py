34dcf625db16a3ed955950f0359321f729e9664e
JatinChhabra07 <chhabrajatin0073@gmail.com>
2026-01-16 19:43:24 +0530

    fixed all issues and error in langchain and api key

--- app/rag.py
+++ app/rag.py
@@ -1,25 +1,27 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_groq import ChatGroq
 from langchain.messages import HumanMessage
-from dotenv import load_dotenv
-import os
 
 
-
-def load_and_chunk_pdf(pdf_path:str):
+def load_and_chunk_pdf(pdf_path: str):
     loader = PyPDFLoader(pdf_path)
     documents = loader.load()
 
     splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 800,
-        chunk_overlap = 200
+        chunk_size=800,
+        chunk_overlap=200
     )
 
     chunks = splitter.split_documents(documents)
     return chunks
 
-def answer_question(vectorstore, question:str):
+
+def answer_question(vectorstore, question: str):
     docs = vectorstore.similarity_search(question, k=3)
 
     context = ""
@@ -28,23 +30,33 @@ def answer_question(vectorstore, question:str):
     for doc in docs:
         page = doc.metadata.get("page")
         pages.add(page)
-        context+=f"(Page {page}): {doc.page_content}\n\n"
+        context += f"(Page {page}): {doc.page_content}\n\n"
 
-        llm = ChatGroq(api_key=os.getenv("GROQ_API_KEY"),
-                       model="llama3-70b-8192",
-                       temperature=0)
+    llm = ChatGroq(
+        api_key=os.getenv("GROQ_API_KEY"),
+        model="llama-3.1-8b-instant",
+        temperature=0
+    )
 
-        prompt = f"""
-        Answer the question using ONLY the context below.
-        Always mention the page number in the answer.
+    prompt = f"""
+    You are an expert assistant answering questions from a document.
 
-        Context:
-        {context}
+    Instructions:
+    - Use ONLY the provided context
+    - Give a detailed and clear explanation
+    - Explain in 3–5 sentences
+    - If applicable, use bullet points
+    - Always mention the page number(s) in the answer
 
-        Question:
-        {question}
-        """
+    Context:
+    {context}
 
-        response = llm([HumanMessage(content=prompt)])
+    Question:
+    {question}
 
-        return response.content, sorted(pages)
+    Answer:
+    """
+
+    response = llm.invoke([HumanMessage(content=prompt)])
+
+    return response.content, sorted(pages)
